7cb683020fca808eb1a45462c9acc59234253484
Matthew Knippen <matthew@zwiffer.com>
2020-04-29 19:47:13 -0500

    sign in with custom components and buttons

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import FormInput from '../../components/form-input/form-input.component'
+import CustomButton from '../../components/custom-button/custom-button.component'
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSumbit = event => {
+        event.preventDefault()
+        this.setState({email:'', password:''})
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target
+        this.setState({[name]: value })
+    }
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSumbit}>
+                    <FormInput name='email' type='email' value={this.state.email} handleChange={this.handleChange} label='Email' required />
+                    <FormInput name='password' type='password' value={this.state.password} onChange={this.handleChange} label='Password' required />
+
+
+                    <CustomButton type='submit' value='Submit Form'>Sign In</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
